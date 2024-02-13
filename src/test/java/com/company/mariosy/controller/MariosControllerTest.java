3a093aaea7403cb66ecd47ac07b335be02491bc5
Bartek <szymk.b9@gmail.com>
2024-02-13 21:08:11 +0100

    chore: delete unnecessary lines

--- src/test/java/com/company/mariosy/controller/MariosControllerTest.java
+++ src/test/java/com/company/mariosy/controller/MariosControllerTest.java
@@ -28,24 +28,3 @@ public class MariosControllerTest {
     @InjectMocks
     MariosController mariosController;
 }
-
-
-//    @Test
-//    @Transactional
-//    public void shouldBeMoreMarioses(){
-//        //given
-//        ControllerTestHelper.createUsersToGivenId(2, userController);
-//
-//        Long creatorId = 1L;
-//        Long receiverId = 2L;
-//
-//        int mariosesCount = mariosController.getAllMarioses().size();
-//        Set<Long> receiversIds = new HashSet<Long>(Sets.newHashSet(receiverId));
-//        MariosDTO mariosDTO = ControllerTestHelper.createMariosDTO(creatorId, receiversIds);
-//        //when
-//        ResponseEntity<String> responseEntity =  mariosController.createMarios(mariosDTO);
-//        int newMariosesCount = mariosController.getAllMarioses().size();
-//        //then
-//        Assertions.assertEquals(mariosesCount+1, newMariosesCount);
-//    }
-
